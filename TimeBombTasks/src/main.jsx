7d731c6d0e2ebe42c1cc81afa820ab8d2630e4ec
Noah-ke24 <timgryziec19@gmail.com>
2026-02-01 12:51:50 -0800

    update

--- TimeBombTasks/src/main.jsx
+++ TimeBombTasks/src/main.jsx
@@ -1,16 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-import App from './App.jsx'
-import { GameContextProvider } from './Context/GameContext.jsx'
+//import { jsxDEV } from "react/jsx-dev-runtime";
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { GameProvider } from "./Context/GameContext";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <GameContextProvider>
-        <App />
-      </GameContextProvider>
-    </BrowserRouter>
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <GameProvider>
+      <App />
+    </GameProvider>
+  </React.StrictMode>
+);
